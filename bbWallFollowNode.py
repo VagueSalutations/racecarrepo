d5eb59d8957f83f9dc5e3c993e52f7c3a07ba910
Sunspark <sunspark@dhcp-18-111-20-85.dyn.mit.edu>
2016-07-14 11:51:51 -0700

    STRUCTURE UPDATE
    We’ve created the master structure; all nodes initialized in node files
    and all calculations performed by the racecar.py class

---
+++ bbWallFollowNode.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+
+"""
+bbWallFollowNode.py
+
+MIT RACECAR 2016
+
+This program implements the bbWallFollow (bang bang
+wall follow) method defined in the racecar.py class.
+
+"""
+
+
+# IMPORTS
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+import rospy
+import math
+import racecar.py
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
+
+# VARIABLES
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––
+SUBSCRIBE_TO_THREAD = "racecar/laser/scan"
+NODE_NAME = 'bbWallFollow'
+
+D_DESIRED = 0.5
+SPEED = 1.0
+
+racecar = racecar()
+
+
+# CALLBACK
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+def callBack(msg):
+
+    racecar.bbWallFollow(msg.ranges, D_DESIRED, SPEED)
+
+
+
+# MAIN()      
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+scanResult = rospy.Subscriber(SUBSCRIBE_TO_THREAD,LaserScan,callBack)
+rospy.init_node(NODE_NAME)
+rospy.spin()
---
+++ racecar.py
@@ -0,0 +1,80 @@
+
+"""
+racecar.py
+
+MIT RACECAR 2016
+
+This class will import all algorithm modules and
+should be imported by the main python script that
+activates the node.
+
+"""
+
+#–––––––––––––––––––––––––––––––––––
+# IMPORTS
+#–––––––––––––––––––––––––––––––––––
+
+import rospy
+import math
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
+#–––––––––––––––––––––––––––––––––––
+# CLASS DECLARATION
+#–––––––––––––––––––––––––––––––––––
+
+class racecar:
+
+    def __init__(self):
+        
+        self.DrivePub = rospy.Publisher('/navigation', AckermannDriveStamped,queue_size=10)
+        self.SafetyPub = rospy.Publisher('/safety', AckermannDriveStamped,queue_size=10)        
+
+        # Add any other topic variables here
+
+
+    #––––––––––––––––––––––––––––––––––––––––––––––––
+    # Function: drive
+    # Parameters: speed (float), angle (float)
+    #
+    # This function will publish a drive command to
+    # the /navigation topic in ROS
+    #––––––––––––––––––––––––––––––––––––––––––––––––
+    def drive(self, speed, angle):
+	msg = AckermannDriveStamped()           # Initializes msg variable
+	msg.drive.speed = speed                 # Sets msg speed to entered speed
+    	msg.drive.acceleration = 0              # Sets msg acceleration to 0
+	msg.drive.jerk = 1                      # Sets msg jerk to 1
+	msg.drive.steering_angle = angle        # Sets msg steering angle to entered angle
+	msg.drive.steering_angle_velocity = 1   # Sets msg angle velocity to 1
+	self.DrivePub.publish(msg)              # Publishes the message
+
+
+    #––––––––––––––––––––––––––––––––––––––––––––––––
+    # Function: bbWallFollow
+    # Parameters: ranges (list), d_desired (float), speed (float)
+    #
+    # This function uses a bang-bang control system
+    # to let the car follow a line
+    #––––––––––––––––––––––––––––––––––––––––––––––––
+    def bbWallFollow(self, ranges, d_desired, speed):
+
+        distance = min(ranges)          # Finds the minimum range
+	error = d_desired - distance    # Calculates error
+        steering_angle = 0              # Initializes steering_angle
+
+        THRESHOLD = 0.1                 # Sets threshold to ±0.1m
+        
+	if abs(error) < THRESHOLD:      # If error within threshold:
+	    steering_angle = 0          #       Kill steering
+
+        elif error > 0:                 # If too far to the right:
+            steering_angle = 0.5        #       Turn left
+
+	elif error < 0:                 # If too far to the left:
+	    steering_angle = -0.5       #       Turn right
+
+	self.drive(speed, steering_angle)    # Execute drive function
+
+
